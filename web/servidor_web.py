50df662011e8543fdfae33e875a5846935db819f
diegomed1411 <37563670+diegomed1411@users.noreply.github.com>
2021-10-29 19:53:26 -0300

    render de mail en inicio
    pasaje de variables desde la funcion y desde session en servidor web

--- web/servidor_web.py
+++ web/servidor_web.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, redirect, url_for, render_template
 from web.servicios import autenticacion
+from flask import session
+from datos.modelos.usuarioResidente import obtener_usuarios_por_email_clave
+
 
 app= Flask(__name__)
 
 @app.route('/')
 def index():
     return redirect(url_for('login'))
+
+usuario = []
+def obtenerDatosUsuario(email, clave):
+    return usuario.append(obtener_usuarios_por_email_clave(email, clave))
+
 
 @app.route('/login', methods= ['GET', 'POST'])
 def login():
@@ -14,7 +22,11 @@ def login():
         if not autenticacion.validar_credenciales(request.form['email'], request.form['clave']):
             error = 'Credenciales inválidas'
         else:
-            return redirect(url_for('inicio'))
+            session['email']= request.form['email']
+            session['clave']= request.form['clave']
+            #return redirect(url_for('inicio' , encabezado = session['email']))
+            datos = obtenerDatosUsuario(session['email'], session['clave'])
+            return render_template('inicio.html', encabezado = datos)
     return render_template('login.html', error=error)
 
 @app.route('/registro', methods = ['GET', 'POST'])
@@ -32,6 +44,7 @@ def inicio():
     usuarios = autenticacion.obtener_usuarios()
     return render_template('inicio.html', usuarios=usuarios)
 
+app.secret_key='hola'
 if __name__ == '__main__':
     app.debug = True
     app.run(port=5002)
